a01fd319bcccb556ea43a64fdb00e0422d8f222e
Kelvin <68477088+DankGuy@users.noreply.github.com>
2023-09-10 00:41:15 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.c9pulvddhfux.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "kelvineeweikeong-employee"
+customregion = "us-east-1"
